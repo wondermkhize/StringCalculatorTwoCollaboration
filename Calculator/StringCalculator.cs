6447b2dc019b25a6df1a9b93980e737d95d0922f
Nombuh <sthelonombusor@gmail.com>
2021-03-09 17:26:50 +0200

    Added negative number testcase

--- Calculator/StringCalculator.cs
+++ Calculator/StringCalculator.cs
@@ -7,12 +7,14 @@ namespace Calculator
         private readonly IDelimiterGetter _delimiter;
         private readonly IStringToIntConversion _parser;
         private readonly ISumOfNumbersLessThanMax _theSum;
+        private readonly INumbersValidator _validations;
 
-        public StringCalculator(IDelimiterGetter _delimiter,IStringToIntConversion _parser,ISumOfNumbersLessThanMax _theSum)
+        public StringCalculator(IDelimiterGetter _delimiter,IStringToIntConversion _parser,ISumOfNumbersLessThanMax _theSum,INumbersValidator _validations)
         {
           this._delimiter = _delimiter;
           this._parser = _parser;
-          this._theSum = _theSum;   
+          this._theSum = _theSum;  
+          this._validations = _validations; 
         }
         
         public int Subtract(string numbers)
@@ -26,6 +28,7 @@ namespace Calculator
         {
             var delimiters = _delimiter.GetDelimiters(numbers);
             var parsedNumbers = _parser.GetNumbers(numbers, delimiters);
+            var validatedNumbers = _validations.GetAbsoluteNumbers(parsedNumbers);
 
             return _theSum.GetSumOfNumbers(parsedNumbers);
         }
